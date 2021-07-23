e42f61de77e1b9cc51295c8176e0434e9399f02b
apple-yagi <applesryu@gmail.com>
2021-07-23 18:57:27 +0900

    return token with user info

--- lib/phx_app_web/views/api/auth/jwt_view.ex
+++ lib/phx_app_web/views/api/auth/jwt_view.ex
@@ -1,8 +1,10 @@
 defmodule PhxAppWeb.Api.Auth.JwtView do
   use PhxAppWeb, :view
+  alias PhxAppWeb.Api.V1.UserView
 
-  def render("sign_in.json", %{user: _user, jwt: jwt}) do
+  def render("sign_in.json", %{user: user, jwt: jwt}) do
     %{token: jwt}
+    |> Map.put_new(:user, render_one(user, UserView, "user.json"))
   end
 
   def render("error.json", %{message: msg}) do
